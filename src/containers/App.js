145a08239943167fa38f1aff609671e6deafec8e
Avutzhan Dautov <dautov92@list.ru>
2021-04-22 11:07:23 +0600

    add method get to fetch

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,7 +15,9 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch('https://jsonplaceholder.typicode.com/users', {
+      method: 'GET'
+    })
       .then(response => response.json())
       .then(users => this.setState({ robots: users}))
     
