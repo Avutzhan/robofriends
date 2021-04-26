e55352f855c457a6c2efa9b0a794735099d1f9ff
Avutzhan Dautov <dautov92@list.ru>
2021-04-26 12:06:11 +0600

    add PWA service workers

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import { searchRobots, requestRobots } from "./reducer";
@@ -20,6 +21,8 @@ ReactDOM.render(<Provider store={store}>
                     <App />
                 </Provider>, document.getElementById('root'));
 
+
+serviceWorkerRegistration.unregister();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
