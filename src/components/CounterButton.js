be8832fc02911b11199b12a4344526d07da55619
Avutzhan Dautov <dautov92@list.ru>
2021-04-28 10:04:46 +0600

    add test and refactor CounterButton to 100% test coverage add CardList test

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -1,30 +1,19 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
-class CounterButton extends Component {
+class CounterButton extends PureComponent {
     constructor() {
         super();
         this.state = {
             count: 0
         }
     }
-    shouldComponentUpdate(nextProps, nextState, nextContext) {
-        if (this.state.count !== nextState.count) {
-            return true
-        }
-        return false
-    }
-
-    updateCount = () => {
-        this.setState(state => {
-            return {count: state.count  + 1}
-        })
-    }
 
     render() {
         return (
             <button
+                id='counter'
                 color={this.props.color}
-                onClick={this.updateCount}>
+                onClick={() => this.setState(state => ({count: state.count  + 1}))}>
                 Count: {this.state.count}
             </button>
         );
