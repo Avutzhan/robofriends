ba81a8c839fa91edffdfc32b9519a3a9fe409fa6
Avutzhan Dautov <dautov92@list.ru>
2021-05-11 11:19:30 +0600

    security note

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -9,6 +9,7 @@ import './MainPage.css';
 
 class MainPage extends Component {
     componentDidMount() {
+        console.log(process.env.REACT_APP_SAY_HI);
         this.props.onRequestRobots()
     }
 
